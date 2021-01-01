a306ccb8d779f7b62421e93d445cd474973dbd86
Test <bnm961126@gmail.com>
2021-01-01 22:50:30 +0900

    login system

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ var passport = require('passport');
 var session = require('express-session');
 
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 
 const opt = {
   key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
+  cert: fs.readFileSync('./ssl/cert.pem'),
+  hostname: 'localhost',
+  port: '3000'
 };
 
 app.set('view engine','ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(session({secret:'MySecret',resave:false,saveUninitialized:true}));
 
 app.use(passport.initialize());
@@ -23,6 +27,9 @@ app.use('/',require('./routes/main.js'));
 app.use('/auth',require('./routes/auth.js'));
 
 var port = 3000;
-https.createServer(opt,app).listen(port,()=>{
-console.log("Express server has started on port 3000")
+// https.createServer(opt,app).listen(port,()=>{
+// console.log("Express server has started on port 3000")
+// });
+app.listen(port,()=>{
+  console.log("Express server has started on port 3000")
 });
