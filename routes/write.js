e430e863f7e6d08321b3151cd0aad3311996651d
dolcelatte <bnm961126@gmail.com>
2021-01-21 22:22:24 -0800

    add mysql

--- routes/write.js
+++ routes/write.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  database : 'post'
+});
+
+connection.connect();
+
 router.get('/',function(req,res){
       res.render('write', {
       user : req.user
       });
 
  });
+
+ router.post('/write', function(req,res,next){
+    var name = req.body.name;
+    var title = req.body.title;
+    var content = req.body.content;
+    var passwd = req.body.passwd;
+    var datas = [name,title,content,passwd];
+
+
+    var sql = "insert into board(name, title, content, regdate, modidate, passwd,hit) values(?,?,?,now(),now(),?,0)";
+    conn.query(sql,datas, function (err, rows) {
+        if (err) console.error("err : " + err);
+        res.redirect('/techReview');
+    });
+});
+
 
 module.exports = router;
