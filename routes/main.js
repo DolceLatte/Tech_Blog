0bb3b5f125309e5a3c1d9339c88da414dc52b2d2
kimjeongwoo <kimjeongwoo@kimjeongwooui-MacBookAir.local>
2021-01-29 17:05:28 +0900

    add model

--- routes/main.js
+++ routes/main.js
@@ -5,26 +5,27 @@ router.use((req,res,next)=>{
       res.locals.user = null;
       res.locals.followerCount = 0;
       res.locals.followerList = [];
+      res.locals.user = req.user
       next();
 });
 
 router.get('/',function(req,res){
       res.render('main', {
-      user : req.user
+      
       });
 
  });
 
 router.get('/techReview',function(req,res){
       res.render('techReview', {
-      user : req.user
+      
       });
 
  });
 
 router.get('/Api',function(req,res){
       res.render('Api', {
-      user : req.user
+      
       });
 
  });
